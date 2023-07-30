44898363ff96a8e56ca45a231810fcaaa35288ac
ge-naks <georgewnakhla@gmail.com>
2023-07-30 19:13:30 -0400

    updates
    added extra functionality to track periods survived.

--- gui-simulation.py
+++ gui-simulation.py
@@ -11,6 +11,7 @@ def gen_t(T, dT):
 def gen_x(initial_wealth, T, dT, a, b, B):
     x = [initial_wealth]
     overflow = [0]
+    period = 500
     for i in range(1, int(T/dT)):
         curr_x = 0
         curr_overflow = 0
@@ -22,38 +23,49 @@ def gen_x(initial_wealth, T, dT, a, b, B):
         if curr_x > B:
             curr_overflow = curr_x - B
             overflow.append(curr_overflow)
-
             curr_x = B
-
             x.append(curr_x)
         elif curr_x > 0 and curr_x < B:
             x.append(curr_x)
             overflow.append(0)
         elif curr_x < 0:
             overflow.append(0)
+            period = i
             break
     
-    return [x, overflow]
+    return [x, overflow, period]
     
 
 def run_B(initial_wealth, T, dT, a, b, B_vals):
     num_runs = 100
     all_overflow = []
-
+    all_periods = []
+    
     for i in range(num_runs):
         B = B_vals[i]
         temp = gen_x(initial_wealth, T, dT, a, b, B)
         all_overflow.append(sum(temp[1]))
-    return all_overflow
+        all_periods.append(temp[2])
+    return [all_overflow, all_periods]
 
+def run_B_for_avg(initial_wealth, T, dT, a, b, B):
+    num_runs = 100
+    all_overflow = []
+    all_periods = []
+    
+    for i in range(num_runs):
+        temp = gen_x(initial_wealth, T, dT, a, b, B)
+        all_overflow.append(sum(temp[1]))
+        all_periods.append(temp[2])
+    return [all_overflow, all_periods]
 B_vals = [i * .1 for i in range(100)]
 
 def sim_wealth(initial_wealth, T, dT, a, b, B):
     t = gen_t(T, dT)
-
     re = gen_x(initial_wealth, T, dT, a, b, B)
     x = re[0]
     overflow = re[1]
+
 
     while(len(x) != int(T/dT)):
         x.append(0)
@@ -61,37 +73,61 @@ def sim_wealth(initial_wealth, T, dT, a, b, B):
     while(len(overflow) != int(T/dT)):
         overflow.append(0)
     total_overflow = sum(overflow)
-    
-    fig, ax = plt.subplots()  # Create a figure and axes object
-    ax.plot(t, x)  # Plot the data on the axes
-
-    # Optionally, you can add labels and a title to the plot
+    fig, ax = plt.subplots() 
+    ax.plot(t, x)  
     ax.set_xlabel('T')
     ax.set_ylabel('Wealth')
     ax.set_title("Dynamic Wealth Evolution")
 
     st.pyplot(fig)
-    st.text("Totale Drawdown produced:" + str(total_overflow))
+    st.text("Total Drawdown produced:" + str(total_overflow))
+
+def sim_avg_per(initial_wealth, T, dT, a, b, B_vals, n):
+   all_period = [i * 0 for i in range(100)]
+   for i in range(n):
+       temp = run_B(initial_wealth, T, dT, a, b, B_vals)
+       avg_per = temp[1]
+       for j in range(len(avg_per)):
+           all_period[j] = all_period[j] + avg_per[j]
+   for i in range(len(all_period)):
+       all_period[i] = all_period[i] / n
+   fig, ax = plt.subplots()  
+   ax.plot(B_vals, all_period)  
+   ax.set_xlabel('B Values')
+   ax.set_ylabel('Average Periods Survived')
+   ax.set_title('Wealth Survivability')
+   st.pyplot(fig)
 
 def sim_B(initial_wealth, T, dT, a, b, B_vals, n):
-
     avg_overflow = [i * 0 for i in range(100)]
-
-    
-
     for i in range(n):
-        all_overflow = run_B(initial_wealth, T, dT, a, b, B_vals)
+        temp = run_B(initial_wealth, T, dT, a, b, B_vals)
+        all_overflow = temp[0]
         for j in range(len(all_overflow)):
-            avg_overflow[j] =  avg_overflow[j] + all_overflow[j]
+            avg_overflow[j] = avg_overflow[j] + all_overflow[j]
 
     for i in range(len(all_overflow)):
         avg_overflow[i] = avg_overflow[i] / n
     
     max_x = max(avg_overflow)
     optimal_B = B_vals[avg_overflow.index(max_x)]
+    temp = run_B_for_avg(initial_wealth, T, dT, a, b, optimal_B)
+    data = temp[1]
 
-    fig, ax = plt.subplots()  # Create a figure and axes object  
-    ax.plot(B_vals, avg_overflow)  # Plot the data on the axes
+    bin_width = 50
+    bins = range(0, max(data) + bin_width, bin_width)
+    counts, _ = np.histogram(data, bins=bins)
+
+    plt.bar(bins[:-1], counts, width=bin_width)
+    plt.xlabel('Number of Survived Periods')
+    plt.ylabel('Frequency')
+    plt.title('B* Period Frequency')
+    plt.xticks(fontsize=8)
+
+    st.pyplot(plt)  # Display the histogram
+
+    fig, ax = plt.subplots()  
+    ax.plot(B_vals, avg_overflow)  
     ax.set_xlabel('B Values')
     ax.set_ylabel('Average Overflow')
     ax.set_title('B* Optimization')
@@ -102,7 +138,8 @@ def sim_B(initial_wealth, T, dT, a, b, B_vals, n):
 
 st.title("Parameterization Tool")
 
-st.subheader("Some sample values: Initial Wealth = 1, a = 1, T = 5 b = 1, dT = .01, n = 100, B = 3.5")
+st.subheader("Some sample values have been already provided. You may change them as you wish!")
+st.subheader("Note: values of n > 1000 will take a significant amount of time to process. Please be patient")
 
 
 
@@ -113,8 +150,9 @@ b = st.number_input("Enter b", value=(1))
 dT = st.number_input("Enter dT" , value=(.01))
 n = st.number_input("Enter Simulation count", value=(100))
 B = st.number_input("Enter B value", value=(2.5))
-st.button("run", on_click=sim_B, args= (iw, T, dT, a, b, B_vals, int(n)))
-st.button("run instance", on_click=sim_wealth, args= (iw, T, dT, a, b, B))
+st.button("Optimize B", on_click=sim_B, args= (iw, T, dT, a, b, B_vals, int(n)))
+st.button("Test One B", on_click=sim_wealth, args= (iw, T, dT, a, b, B))
+st.button("Simulate Average Survival", on_click=sim_avg_per, args= (iw, T, dT, a, b, B_vals, n))
 
 st.write("Made by George Nakhla")
 
--- simulation.py
+++ simulation.py
@@ -11,6 +11,7 @@ def gen_t(T, dT):
 def gen_x(initial_wealth, T, dT, a, b, B):
     x = [initial_wealth]
     overflow = [0]
+    period = 500
     for i in range(1, int(T/dT)):
         curr_x = 0
         curr_overflow = 0
@@ -22,36 +23,49 @@ def gen_x(initial_wealth, T, dT, a, b, B):
         if curr_x > B:
             curr_overflow = curr_x - B
             overflow.append(curr_overflow)
-
             curr_x = B
-
             x.append(curr_x)
         elif curr_x > 0 and curr_x < B:
             x.append(curr_x)
             overflow.append(0)
         elif curr_x < 0:
             overflow.append(0)
+            period = i
             break
     
-    return [x, overflow]
+    return [x, overflow, period]
     
 
 def run_B(initial_wealth, T, dT, a, b, B_vals):
     num_runs = 100
     all_overflow = []
-
+    all_periods = []
+    
     for i in range(num_runs):
         B = B_vals[i]
         temp = gen_x(initial_wealth, T, dT, a, b, B)
         all_overflow.append(sum(temp[1]))
-    return all_overflow
+        all_periods.append(temp[2])
+    return [all_overflow, all_periods]
+
+
+def run_B_for_avg(initial_wealth, T, dT, a, b, B):
+    num_runs = 100
+    all_overflow = []
+    all_periods = []
+    
+    for i in range(num_runs):
+        temp = gen_x(initial_wealth, T, dT, a, b, B)
+        all_overflow.append(sum(temp[1]))
+        all_periods.append(temp[2])
+    return [all_overflow, all_periods]
 
 def sim_wealth(initial_wealth, T, dT, a, b, B):
     t = gen_t(T, dT)
-
     re = gen_x(initial_wealth, T, dT, a, b, B)
     x = re[0]
     overflow = re[1]
+
 
     while(len(x) != int(T/dT)):
         x.append(0)
@@ -59,15 +73,11 @@ def sim_wealth(initial_wealth, T, dT, a, b, B):
     while(len(overflow) != int(T/dT)):
         overflow.append(0)
     total_overflow = sum(overflow)
-    
     fig, ax = plt.subplots() 
     ax.plot(t, x)  
     ax.set_xlabel('T')
     ax.set_ylabel('Wealth')
     ax.set_title("Dynamic Wealth Evolution")
-    txt = "Total Drawdown produced:" + str(total_overflow)
-    fig.text(.5, .005, txt, ha = 'center')
-    
     
     #these commented out lines will automatically make the graphs 
     # full screen so that the text at the bottom is not squished, 
@@ -77,14 +87,30 @@ def sim_wealth(initial_wealth, T, dT, a, b, B):
     #manager.full_screen_toggle()
     
     plt.show()
+
+
+def sim_avg_per(initial_wealth, T, dT, a, b, B_vals, n):
+   all_period = [i * 0 for i in range(100)]
+   for i in range(n):
+       temp = run_B(initial_wealth, T, dT, a, b, B_vals)
+       avg_per = temp[1]
+       for j in range(len(avg_per)):
+           all_period[j] = all_period[j] + avg_per[j]
+   for i in range(len(all_period)):
+       all_period[i] = all_period[i] / n
+   fig, ax = plt.subplots()  
+   ax.plot(B_vals, all_period)  
+   ax.set_xlabel('B Values')
+   ax.set_ylabel('Average Periods Survived')
+   ax.set_title('Wealth Survivability')
+   plt.show()
+
+
 def sim_B(initial_wealth, T, dT, a, b, B_vals, n):
-
     avg_overflow = [i * 0 for i in range(100)]
-
-    
-
     for i in range(n):
-        all_overflow = run_B(initial_wealth, T, dT, a, b, B_vals)
+        temp = run_B(initial_wealth, T, dT, a, b, B_vals)
+        all_overflow = temp[0]
         for j in range(len(all_overflow)):
             avg_overflow[j] =  avg_overflow[j] + all_overflow[j]
 
@@ -93,7 +119,16 @@ def sim_B(initial_wealth, T, dT, a, b, B_vals, n):
     
     max_x = max(avg_overflow)
     optimal_B = B_vals[avg_overflow.index(max_x)]
+    temp = run_B_for_avg(initial_wealth, T, dT, a, b, optimal_B)
+    data = temp[1]
 
+
+    counts, bins = np.histogram(data)
+    plt.stairs(counts, bins)
+    plt.xlabel('Number of Survived Periods')
+    plt.ylabel('Frequency')
+    plt.title('B* Period Frequency')
+    plt.xticks(fontsize=5)
     fig, ax = plt.subplots()  
     ax.plot(B_vals, avg_overflow)  
     ax.set_xlabel('B Values')
@@ -114,6 +149,32 @@ def sim_B(initial_wealth, T, dT, a, b, B_vals, n):
     #manager.full_screen_toggle()
     plt.show()
 
+def binary_search(arr, low, high, x):
+ 
+    # Check base case
+    if high >= low:
+ 
+        mid = (high + low) // 2
+ 
+        # If element is present at the middle itself
+        if arr[mid] == x:
+            return True
+ 
+        # If element is smaller than mid, then it can only
+        # be present in left subarray
+        elif arr[mid] > x:
+            return binary_search(arr, low, mid - 1, x)
+ 
+        # Else the element can only be present in right subarray
+        else:
+            return binary_search(arr, mid + 1, high, x)
+ 
+    else:
+        # Element is not present in the array
+        return False
+
+
+
 
 
 #do not change
@@ -125,11 +186,12 @@ T = 5
 dT = .01
 a = 1
 b = 1
-n = 10
-B = 2
+n = 100
+B = 2.5
 
-
+#sim_avg_per(initial_wealth, T, dT, a, b, B_vals, n)
 sim_B(initial_wealth , T, dT, a, b, B_vals, n)
-sim_wealth(initial_wealth, T, dT, a, b, B)
+#sim_wealth(initial_wealth, T, dT, a, b, B)
+
 
 
