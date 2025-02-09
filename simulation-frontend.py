c09d82dab8bdd686446a7f504d3d79a81fba7c1f
George Nakhla <105831852+ge-naks@users.noreply.github.com>
2025-02-09 01:22:28 -0500

    total refactor

---
+++ simulation-frontend.py
@@ -0,0 +1,39 @@
+import streamlit as st
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from simulation import gen_one_plt, optimize_ceiling
+
+sns.set_theme(style="whitegrid")
+
+
+def clear_plots():
+    if 'fig' in st.session_state:
+        plt.close(st.session_state.fig)
+
+def main():
+    st.title("Wealth Simulation")
+    st.subheader("Made by George Nakhla")
+    initial_wealth = st.number_input("Initial Wealth", value=1)
+    T = st.number_input("Total Time (T)", value = 5, min_value=1)
+    dT = st.number_input("Time Delta (dT)", value=0.01, min_value=0.005)
+    a = st.number_input("Parameter a", value=1)
+    b = st.number_input("Parameter b", value=1)
+    ceiling = st.number_input("Ceiling", value=2, min_value=1)
+    N = st.number_input("Number of simulations for optimization", value=100, min_value=1)
+
+    if st.button("Run Single Wealth Simulation"):
+        clear_plots()
+        gen_one_plt(initial_wealth, T, dT, a, b, ceiling)
+        st.session_state.fig = plt.gcf()
+        st.pyplot(st.session_state.fig)
+
+    # Run the optimize_ceiling function
+    if st.button("Run Ceiling Optimization"):
+        clear_plots()  # Clear any existing plots first
+        optimize_ceiling(initial_wealth, T, dT, a, b, N)
+        st.session_state.fig = plt.gcf()
+        st.pyplot(st.session_state.fig)
+
+if __name__ == "__main__":
+    main()
--- simulation.py
+++ simulation.py
@@ -1,197 +1,83 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-def gen_t(T, dT):
-    t = []
-    for i in range(int(T/dT)):
-        t.append(i)
-    return t
+sns.set_theme(style="whitegrid")
 
+def gen_wealth(initial_wealth, T, dT, a, b, ceiling):
+    periods_survived = int(T / dT)
+    wealth = np.zeros(periods_survived)
+    wealth[0] = initial_wealth
+    overflow = 0
+    noise = np.random.normal(0, np.sqrt(dT), periods_survived)
+    for i in range(1, periods_survived):
+        dX = a * dT + b * noise[i]
+        curr_wealth = wealth[i - 1] + dX
 
-def gen_x(initial_wealth, T, dT, a, b, B):
-    x = [initial_wealth]
-    overflow = [0]
-    period = int(T/dT)
-    for i in range(1, int(T/dT)):
-        curr_x = 0
-        curr_overflow = 0
-        dW = np.random.normal(0, np.sqrt(dT), 1)
-        dW = dW[0]
-        dX = a * dT + b * dW
-        curr_x = x[i-1] + dX
-
-        if curr_x > B:
-            curr_overflow = curr_x - B
-            overflow.append(curr_overflow)
-            curr_x = B
-            x.append(curr_x)
-        elif curr_x > 0 and curr_x < B:
-            x.append(curr_x)
-            overflow.append(0)
-        elif curr_x < 0:
-            overflow.append(0)
-            period = i
-            break
-    
-    return [x, overflow, period]
-    
-
-def run_B(initial_wealth, T, dT, a, b, B_vals):
-    num_runs = 100
-    all_overflow = []
-    all_periods = []
-    
-    for i in range(num_runs):
-        B = B_vals[i]
-        temp = gen_x(initial_wealth, T, dT, a, b, B)
-        all_overflow.append(sum(temp[1]))
-        all_periods.append(temp[2])
-    return [all_overflow, all_periods]
-
-
-def run_B_for_avg(initial_wealth, T, dT, a, b, B):
-    num_runs = 100
-    all_overflow = []
-    all_periods = []
-    
-    for i in range(num_runs):
-        temp = gen_x(initial_wealth, T, dT, a, b, B)
-        all_overflow.append(sum(temp[1]))
-        all_periods.append(temp[2])
-    return [all_overflow, all_periods]
-
-def sim_wealth(initial_wealth, T, dT, a, b, B):
-    t = gen_t(T, dT)
-    re = gen_x(initial_wealth, T, dT, a, b, B)
-    x = re[0]
-    overflow = re[1]
-
-
-    while(len(x) != int(T/dT)):
-        x.append(0)
-
-    while(len(overflow) != int(T/dT)):
-        overflow.append(0)
-    total_overflow = sum(overflow)
-    fig, ax = plt.subplots() 
-    ax.plot(t, x)  
-    ax.set_xlabel('T')
-    ax.set_ylabel('Wealth')
-    ax.set_title("Dynamic Wealth Evolution")
-    
-    #these commented out lines will automatically make the graphs 
-    # full screen so that the text at the bottom is not squished, 
-    # undesirable behavior that i will fix... eventually
-
-    #manager = plt.get_current_fig_manager()
-    #manager.full_screen_toggle()
-    
-    plt.show()
-
-
-def sim_avg_per(initial_wealth, T, dT, a, b, B_vals, n):
-   all_period = [i * 0 for i in range(100)]
-   for i in range(n):
-       temp = run_B(initial_wealth, T, dT, a, b, B_vals)
-       avg_per = temp[1]
-       for j in range(len(avg_per)):
-           all_period[j] = all_period[j] + avg_per[j]
-   for i in range(len(all_period)):
-       all_period[i] = all_period[i] / n
-   fig, ax = plt.subplots()  
-   ax.plot(B_vals, all_period)  
-   ax.set_xlabel('B Values')
-   ax.set_ylabel('Average Periods Survived')
-   ax.set_title('Wealth Survivability')
-   plt.show()
-
-
-def sim_B(initial_wealth, T, dT, a, b, B_vals, n):
-    avg_overflow = [i * 0 for i in range(100)]
-    for i in range(n):
-        temp = run_B(initial_wealth, T, dT, a, b, B_vals)
-        all_overflow = temp[0]
-        for j in range(len(all_overflow)):
-            avg_overflow[j] =  avg_overflow[j] + all_overflow[j]
-
-    for i in range(len(all_overflow)):
-        avg_overflow[i] = avg_overflow[i] / n
-    
-    max_x = max(avg_overflow)
-    optimal_B = B_vals[avg_overflow.index(max_x)]
-    temp = run_B_for_avg(initial_wealth, T, dT, a, b, optimal_B)
-    data = temp[1]
-
-
-    counts, bins = np.histogram(data)
-    plt.stairs(counts, bins)
-    plt.xlabel('Number of Survived Periods')
-    plt.ylabel('Frequency')
-    plt.title('B* Period Frequency')
-    plt.xticks(fontsize=5)
-    fig, ax = plt.subplots()  
-    ax.plot(B_vals, avg_overflow)  
-    ax.set_xlabel('B Values')
-    ax.set_ylabel('Average Overflow')
-    ax.set_title('B* Optimization')
-
-    txt1 = "Optimal B value at:" + str(optimal_B)
-    txt2 = "Max drawdown of:" + str(max_x)
-
-    fig.text(.5, .005, txt1, ha = 'center')
-    fig.text(.5, .05, txt2, ha = 'center')
-    
-    #these commented out lines will automatically make the graphs 
-    # full screen so that the text at the bottom is not squished, 
-    # undesirable behavior that i will fix... eventually
-
-    #manager = plt.get_current_fig_manager()
-    #manager.full_screen_toggle()
-    plt.show()
-
-def binary_search(arr, low, high, x):
- 
-    # Check base case
-    if high >= low:
- 
-        mid = (high + low) // 2
- 
-        # If element is present at the middle itself
-        if arr[mid] == x:
-            return True
- 
-        # If element is smaller than mid, then it can only
-        # be present in left subarray
-        elif arr[mid] > x:
-            return binary_search(arr, low, mid - 1, x)
- 
-        # Else the element can only be present in right subarray
+        if curr_wealth > ceiling:
+            overflow += curr_wealth - ceiling
+            curr_wealth = ceiling
+            wealth[i] = curr_wealth
+        elif 0 < curr_wealth < ceiling:
+            wealth[i] = curr_wealth
         else:
-            return binary_search(arr, mid + 1, high, x)
- 
-    else:
-        # Element is not present in the array
-        return False
+            periods_survived = i
+            break
+    return [wealth, periods_survived, overflow]
+
+def gen_one_plt(initial_wealth, T, dT, a, b, ceiling):
+    wealth, periods_survived, overflow = gen_wealth(initial_wealth, T, dT, a, b, ceiling)
+    time = [i for i in range(int(T / dT))]
+    plt.suptitle(f'Parameters of a = {a}, b = {b}, ceiling = {ceiling}')
+    plt.title(f'Survived {periods_survived} periods and generated {round(overflow, 3)} overflow')
+    plt.xlabel("Time")
+    plt.ylabel("Wealth")
+    plt.plot(time, wealth)
+    plt.show()
+    return overflow
 
 
+def optimize_ceiling(initial_wealth, T, dT, a, b, N, max_ceiling = 100, ceiling_step = .1):
+    ceilings = [i * ceiling_step for i in range(max_ceiling)]
+    overflow_per_ceiling = np.zeros(max_ceiling)
+    survival_per_ceiling = np.zeros(max_ceiling)
+    optimal_ceiling = 0
+    avg_over_at_opt = 0
+    for i in range(len(ceilings)):
+        curr_ceiling_overflow = 0
+        curr_ceiling_survival = 0
+        for _ in range(N):
+            iter_result = gen_wealth(initial_wealth, T, dT, a, b, ceilings[i])
+            curr_ceiling_overflow += iter_result[2]
+            curr_ceiling_survival += iter_result[1]
+        overflow_per_ceiling[i] = curr_ceiling_overflow / N
+        survival_per_ceiling[i] = curr_ceiling_survival / N
+        if (curr_ceiling_overflow / N ) > avg_over_at_opt:
+            avg_over_at_opt = curr_ceiling_overflow / N
+            optimal_ceiling = ceilings[i]
+    
 
+    plt.figure(figsize=(13, 6))
 
-
-#do not change
-B_vals = [i * .1 for i in range(100)]
-
-# sample values, can change parameterizations
-initial_wealth = 1
-T = 5
-dT = .01
-a = 1
-b = 1
-n = 100
-B = 2.5
-
-#sim_avg_per(initial_wealth, T, dT, a, b, B_vals, n)
-sim_B(initial_wealth , T, dT, a, b, B_vals, n)
-#sim_wealth(initial_wealth, T, dT, a, b, B)
-
-
-
+    # First subplot - Overflow vs Ceiling
+    plt.subplot(1, 3, 1)
+    plt.plot(ceilings, overflow_per_ceiling, color=sns.color_palette("muted")[0])
+    plt.title(f'Optimal ceiling at {round(optimal_ceiling, 1)}. Avg. overflow of {round(avg_over_at_opt, 3)}')
+    plt.xlabel("Ceiling")
+    plt.ylabel("Avg. Overflow")
+    
+    # Second subplot - Survival vs Ceiling
+    plt.subplot(1, 3, 2)
+    plt.plot(ceilings, survival_per_ceiling, color=sns.color_palette("muted")[1])
+    plt.xlabel("Ceiling")
+    plt.ylabel("Avg. Survival")
+    
+    # Third subplot - Histogram of Survival
+    plt.subplot(1, 3, 3)
+    counts, bins = np.histogram(survival_per_ceiling)
+    plt.stairs(counts, bins, color=sns.color_palette("muted")[2])
+    plt.xlabel("Survival")
+    plt.ylabel("Frequency")
+    plt.suptitle('Ceiling vs Avg. Overflow & Avg. Survival')
+    plt.show()
+    return optimal_ceiling
